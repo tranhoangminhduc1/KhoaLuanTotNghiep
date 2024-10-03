c4a38dd8560072c1f90a2da90cc157bcc766f504
unknown <tranhoangminhduc2907203@gmail.com>
2024-10-03 21:48:45 +0700

    Resolved merge conflicts

---
+++ resources/views/admin/app/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+             © 2024 Bản quyền thuộc về:
+             <a class="text-info" href="/ ">MINH DUC SHOP - Minh Đức</a>
+        </div>
+    </div>
+ </footer>
+ 
